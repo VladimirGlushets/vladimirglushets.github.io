5d7a3097c0a9a60a7c74c4aa9d7552578dfd8d7c
MINSK\Uladzimir_Hlushets <Uladzimir_Hlushets@epam.com>
2016-11-17 12:18:48 +0300

    babel works

---
+++ js/lib/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var dataUrl = 'https://newsapi.org/v1/articles?source=techcrunch&apiKey=05d1310787a94c24a707f1c3c8d2584c';
+
+var dataService = new DataService(dataUrl);
+var articleService = new ArticleService();
+
+dataService.getData().then(function (data) {
+
+    // after getting data I converted it into html text
+    var articlesHtml = articleService.getArticlesHtml(data.articles);
+
+    // found container for pushing html articles
+    var articlesContainer = document.getElementsByClassName('articles');
+
+    if (articlesContainer && articlesContainer.length > 0) {
+
+        // pushed articles into container
+        articlesContainer[0].innerHTML = articlesHtml.join(' ');
+    }
+}).catch(function (err) {
+    console.log(err);
+});
---
+++ js/lib/articleService.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+// When I use modules feature it will be a private function (I think)
+var createArticle = function createArticle(articleJson) {
+  return "\n  <div class=\"article-container\">\n      <div class=\"body\">\n          <div class=\"title\">\n            <a href=\"" + articleJson.url + "\" alt=\"Full article\" target=\"_blank\">\n              <h2>" + articleJson.title + "</h2>\n            </a>\n          </div>\n          <div class=\"author\">by " + articleJson.author + "</div>\n          <div class=\"description\">" + articleJson.description + "</div>\n          <div class=\"publish-at\">" + articleJson.publishedAt + "</div>\n      </div>\n      <div class=\"image\">\n        <a href=\"" + articleJson.url + "\" alt=\"Full article\" target=\"_blank\">\n          <img src=\"" + articleJson.urlToImage + "\">\n        </a>\n      </div>\n  </div> ";
+};
+
+// service for converting JSON objects to HTML articles objects
+
+var ArticleService = function () {
+  function ArticleService() {
+    _classCallCheck(this, ArticleService);
+  }
+
+  _createClass(ArticleService, [{
+    key: "getArticlesHtml",
+    value: function getArticlesHtml(articles) {
+      return articles.map(function (el) {
+        return createArticle(el);
+      });
+    }
+  }]);
+
+  return ArticleService;
+}();
