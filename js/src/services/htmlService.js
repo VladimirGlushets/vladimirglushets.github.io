d29c3e28b1059aa7c83c48271d03dd44e7742849
MINSK\Uladzimir_Hlushets <Uladzimir_Hlushets@epam.com>
2016-11-22 10:05:39 +0300

    fixed images size

--- js/src/services/htmlService.js
+++ js/src/services/htmlService.js
@@ -3,11 +3,13 @@ const createThemes = function(themaJson, controller) {
     return `
     <div class="article-container">
       <div class="body">
-        <span class="image">
-          <a href="#" onclick = "controller.fillArticles('${themaJson.id}');" >
-            <img src="${themaJson.urlsToLogos.small}">
-          </a>
-        </span>
+        <div class="image">
+          <div class='floater'>
+            <a href="#" onclick = "controller.fillArticles('${themaJson.id}');" >
+              <img src="${themaJson.urlsToLogos.small}">
+              </a>
+          </div>
+        </div>
 
         <div class="title">
             <a href="#" onclick = "controller.fillArticles('${themaJson.id}');">
@@ -23,20 +25,22 @@ const createArticle = function(articleJson) {
     return `
     <div class="article-container">
       <div class="body">
-        <span class="image">
-          <a href="${articleJson.url}" alt="Full article" target="_blank">
-            <img src="${articleJson.urlToImage}">
-          </a>
-        </span>
+        <div class="image">
+          <div class='floater'>
+            <a href="${articleJson.url}" alt="Full article" target="_blank">
+              <img src="${articleJson.urlToImage}">
+            </a>
+          </div>
+        </div>
 
             <div class="title">
               <a href="${articleJson.url}" alt="Full article" target="_blank">
                 ${articleJson.title}
               </a>
             </div>
-            <div class="author">by ${articleJson.author}</div>
+            <div class="author">${articleJson.author ? 'by ' + articleJson.author : ' '}</div>
             <div class="description">${articleJson.description}</div>
-            <div class="publish-at">${articleJson.publishedAt ? articleJson.publishedAt : ''}</div>
+            <div class="publish-at">${articleJson.publishedAt ? articleJson.publishedAt : ' '}</div>
           </div>
   </div>`;
 }
